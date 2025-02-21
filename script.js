af21a3c7f6748236c2d81a9044f378e1bae225d5
ManVu2110 <manvu2110@gmail.com>
2025-02-21 10:33:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login()
 {
   let u = username.value
   let p = pass.value
-  if (u=="ManVu2110" && p=="mandeptrai2110")
+  if (u=="m" && p=="1")
   {
     window.open("home.html")
     document.getElementById(compiler).style.display="block"
