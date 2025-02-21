c1b0bf9858057a853eb0b2ca5fed9bacf8bf7046
ManVu2110 <manvu2110@gmail.com>
2025-02-21 10:20:12 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = pass.value
+  if (u=="ManVu2110" && p=="mandeptrai2110")
+  {
+    window.open("home.html")
+    document getElementById(compiler).style.display="block"
+  }
+}
